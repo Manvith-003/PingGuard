a979a5c384b0f1116075dc1642d55a4fe1919d16
Manvith-003 <manvithpoojary63@gmail.com>
2026-03-27 17:10:32 +0530

    feat: Add MongoDB connection setup using Mongoose.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("👉 MONGO_URI:", process.env.MONGO_URI);
+
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is undefined ❌");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+
+    console.log("✅ MongoDB connected");
   } catch (err) {
-    console.error(err);
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
